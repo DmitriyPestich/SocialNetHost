781f7520624a1a0538977b07004db27c5b9953cd
Pestich.Dmitriy <dmitriypestich@gmail.com>
2021-02-16 18:05:39 +0200

    Updates

---
+++ precache-manifest.f25d01f8a40445d6d3fb54620b704d65.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b7c6ac1296f91c4bc6e44c536ecc1b",
+    "url": "/DmitriyPestich/SocialNetHost/index.html"
+  },
+  {
+    "revision": "bb455efdb16b31d19fa8",
+    "url": "/DmitriyPestich/SocialNetHost/static/css/3.2ff0a6ba.chunk.css"
+  },
+  {
+    "revision": "cd0c86f11226e161991e",
+    "url": "/DmitriyPestich/SocialNetHost/static/css/4.91905f3b.chunk.css"
+  },
+  {
+    "revision": "2d23f39d7f9618a27f17",
+    "url": "/DmitriyPestich/SocialNetHost/static/css/main.16737d18.chunk.css"
+  },
+  {
+    "revision": "3594efbf09e625910187",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/2.00b98ca2.chunk.js"
+  },
+  {
+    "revision": "1f3ec544f59db43d224e4cd99b0c3b1d",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/2.00b98ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb455efdb16b31d19fa8",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/3.10fe7203.chunk.js"
+  },
+  {
+    "revision": "cd0c86f11226e161991e",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/4.3f9e9034.chunk.js"
+  },
+  {
+    "revision": "2d23f39d7f9618a27f17",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/main.1f316447.chunk.js"
+  },
+  {
+    "revision": "66c1a66addefe6afd43d",
+    "url": "/DmitriyPestich/SocialNetHost/static/js/runtime-main.38679cb3.js"
+  },
+  {
+    "revision": "66ae567527db007b8be429e7e59c2a63",
+    "url": "/DmitriyPestich/SocialNetHost/static/media/preloader.66ae5675.gif"
+  },
+  {
+    "revision": "d3758a57ecc466dc877d8a8fe8853dd9",
+    "url": "/DmitriyPestich/SocialNetHost/static/media/user.d3758a57.png"
+  }
+]);
